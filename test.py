bdd433c708406ecf20ef2d135286bd683f9532c1
Lab User <133401976+JamesSteele977@users.noreply.github.com>
2023-08-31 16:55:26 -0600

    just playing around for now

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import numpy as np
 import copy
 
-def get_nd_input(domains, 
-                 shape: list):
-    
-    xy = np.mgrid[0:1:3.1j, 0:1:3.1j].reshape(2,-1).T
+def get_nd_input(doms: list, 
+                 ns: list):
+    assert len(doms) == len(ns)
+    mgrid_config = [np.s_[dom[0]:dom[1]:n*1j] for dom, n in zip(doms, ns)] 
+    grid = np.mgrid[mgrid_config].reshape(len(ns),-1).T
+    return grid
 
-test = get_nd_input(domains=[(0, 10),
-                             (0, 10)],
-                    shape=[11, 11])
+test = get_nd_input(doms=[(0, 10),
+                          (0, 10),
+                          (0, 10)],
+                    ns=[11, 11, 11])
 
+np.set_printoptions(threshold=np.inf)
 print(test)
