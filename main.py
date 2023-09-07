a89ef0fd9b3572df85ad04c2b99b76c13e945598
Lab User <133401976+JamesSteele977@users.noreply.github.com>
2023-09-06 20:31:22 -0600

    meh

--- main.py
+++ main.py
@@ -15,24 +15,21 @@ subclassing_dict = {
 
 with open(sys.argv[1], 'r') as f:
     inputs = json.load(f)
-with open('settings.json', 'r') as f:
-    settings = json.load(f)
-
 
 sublcass_dir = inputs["sensor_type"].split('_')
 subclassed_sensor = subclassing_dict[sublcass_dir[0]][sublcass_dir[1]][sublcass_dir[2]]
 
-sensor = Sensor(settings=settings,
+sensor = Sensor(settings=inputs["settings"],
                 inputs=inputs,
                 subclassed_sensor=subclassed_sensor)
 
 losses, params, footprints, nonlinearities, sensitivities = sensor._fit()
 
 print(f"""OPTIMIZED PARAMS:
-Width: {params[-1,0]}
-Height: {params[-1,1]}
-Z Dist: {params[-1,2]}
-Length: {params[-1,3]}""")\
+Width: {params[-1,0]*inputs["unit_norm"]}
+Height: {params[-1,1]*inputs["unit_norm"]}
+Z Dist: {params[-1,2]*inputs["unit_norm"]}
+Length: {params[-1,3]*inputs["unit_norm"]}""")\
 
 def normalize(X):
     return (X-np.min(X))/(np.ptp(X))
@@ -41,7 +38,6 @@ fig, ax = plt.subplots(1, 2)
 plt.yscale('log')
 ax[0].plot(losses)
 plt.legend(["loss"])
-plt.yscale('log')
 for parameter in range(params.shape[-1]):
     ax[1].plot(params[:,parameter])
 ax[1].plot(normalize(footprints)*np.max(params), 'r--')
