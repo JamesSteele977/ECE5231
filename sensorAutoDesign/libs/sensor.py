a9830f18224da407349c6bc16719a0bc08be74b9
Lab User <133401976+JamesSteele977@users.noreply.github.com>
2023-11-16 17:11:29 -0700

    Fix: change "staticmethod" to "frozen"

--- sensorAutoDesign/libs/sensor.py
+++ sensorAutoDesign/libs/sensor.py
@@ -9,25 +9,25 @@ trainableVars: type = Tuple[tf.Variable, ...]
 def EvalType(returns: type) -> type:
     return Callable[[trainableVars], returns]
 
-@dataclass(staticmethod=True)
+@dataclass(frozen=True)
 class SensorBasicInfo():
     trainable_variables: Dict[str, Tuple[float, float]]
     bandwidth: Tuple[float, float]
     input_symbol: str
 
-@dataclass(staticmethod=True)
+@dataclass(frozen=True)
 class SensorConfig(SensorBasicInfo):
     parameter_relationships: Tuple[str, ...]
     footprint: str
     response: str
 
-@dataclass(staticmethod=True)
+@dataclass(frozen=True)
 class ParameterRelationship():
     boolean_evaluation: EvalType(bool)
     substitution_solve: EvalType(float)
     sympy_expression: Expr
 
-@dataclass(staticmethod=True)
+@dataclass(frozen=True)
 class SensorProfile(SensorBasicInfo):
     parameter_relationships: Tuple[ParameterRelationship, ...]
     _get_footprint: EvalType(np.float32)
